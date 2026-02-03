1ebfc71f0dc8d893f196c94ea60dd0659f9c4af4
JayKarolia <karolia.jay@ibm.com>
2026-02-03 12:07:25 +0530

    added openrouter base url in env file

--- llm.py
+++ llm.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENROUTER_API_KEY = os.getenv("OPENROUTER-API-KEY")
-OPENROUTER_BASE_URL = "https://openrouter.ai/api/v1"
-MODEL = os.getenv("OPENROUTER-LLM-MODEL")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+OPENROUTER_BASE_URL = os.getenv("OPENROUTER_BASE_URL")
+MODEL = os.getenv("OPENROUTER_LLM_MODEL")
 
 client = OpenAI(
     api_key=OPENROUTER_API_KEY,
